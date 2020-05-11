316beed410d587edd228e78354b37d4c09675806
andytheelf <ballew22@gmail.com>
2020-05-10 20:19:51 -0500

    node commit

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('Hello Node!')
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+console.log('==============')
+const printProfileData = profileDataArr.forEach(profileItem => console.log(profileItem));
